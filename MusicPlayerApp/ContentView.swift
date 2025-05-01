13cc0d377f0287b63e0ec8fe2e52286b87b4e44b
krina-iOS <krina.patel@kombee.com>
2025-05-01 12:45:08 +0530

    Initial Commit

---
+++ MusicPlayerApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MusicPlayerApp
+//
+//  Created by iMac on 01/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MusicPlayerApp/MusicPlayerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerAppApp.swift
+//  MusicPlayerApp
+//
+//  Created by iMac on 01/05/25.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
